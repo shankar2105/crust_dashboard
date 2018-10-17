a70de7fb7537808987ebc71818a5526ede5c4226
ashwin kumar <ashwinkumar2468@gmail.com>
2018-10-17 13:54:03 +0530

    fix/table: reset table paging if filter changes

--- app/components/Tables.jsx
+++ app/components/Tables.jsx
@@ -1,40 +1,61 @@
 import React, { Component } from "react";
-import { Table} from "antd";
+import { Table } from "antd";
+import { isEquivalent } from "../redux/utils";
 
 import columns from '../assets/tableData/tableColumn';
 
 
-function filterLogs(rawData){
+function filterLogs(rawData) {
   const dataSource = [];
   rawData.forEach(log => {
     dataSource.push({
-        key: log.logDataHash,
-        num: log.logDataHash,
-        // direct: log.is_direct_successful ? "Yes" : "No",
-        tcp_hp: log.tcp_hole_punch_result === "Succeeded" ? "Yes" : "Fail",
-        udp_hp: log.udp_hole_punch_result === "Succeeded" ? "Yes" : "Fail",
-        nat_type: [log.peer_requester.nat_type, log.peer_responder.nat_type],
-        os: [log.peer_requester.os, log.peer_responder.os],
-        country: [log.peer_requester.geo_info.country_name, log.peer_responder.geo_info.country_name],
-        isSuccessful: log.isSuccessful,
-        color: (log.tcp_hole_punch_result === "Failed" && log.udp_hole_punch_result === "Failed")? 'table-row-colour-fail' : 'table-row-colour-success'
+      key: log.logDataHash,
+      num: log.logDataHash,
+      // direct: log.is_direct_successful ? "Yes" : "No",
+      tcp_hp: log.tcp_hole_punch_result === "Succeeded" ? "Yes" : "Fail",
+      udp_hp: log.udp_hole_punch_result === "Succeeded" ? "Yes" : "Fail",
+      nat_type: [log.peer_requester.nat_type, log.peer_responder.nat_type],
+      os: [log.peer_requester.os, log.peer_responder.os],
+      country: [log.peer_requester.geo_info.country_name, log.peer_responder.geo_info.country_name],
+      isSuccessful: log.isSuccessful,
+      color: (log.tcp_hole_punch_result === "Failed" && log.udp_hole_punch_result === "Failed") ? 'table-row-colour-fail' : 'table-row-colour-success'
     });
   });
   return dataSource
 }
 
 class Tables extends Component {
+  constructor() {
+    super();
+    this.state = {
+      page: 1,
+    };
+  }
+
+  pageChange(page) {
+    this.setState({
+      page: page
+    });
+  }
+
+  componentWillUpdate(nextProps) {
+    if (!isEquivalent(this.props.dataSource, nextProps.dataSource)) {
+      this.setState({
+        page: 1
+      });
+    }
+  }
+
   render() {
-    const {dataSource} = this.props;
-    const filterData=filterLogs(dataSource)
+    const { dataSource } = this.props;
+    const filterData = filterLogs(dataSource)
     return (
       <div>
-        <Table 
+        <Table
           dataSource={filterData}
           columns={columns}
           rowClassName={(record) => record.color}
-          pagination={true}
-          
+          pagination={{ onChange: this.pageChange.bind(this), current: this.state.page }}
         />
       </div>
     );
